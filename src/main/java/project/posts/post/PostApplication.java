15bccaa0aeb156c925df462b85b366816c9a2fbe
valtteri <valtteri.koivunen@hotmail.com>
2020-07-25 15:14:28 +0100

    comment test data

--- src/main/java/project/posts/post/PostApplication.java
+++ src/main/java/project/posts/post/PostApplication.java
@@ -28,7 +28,7 @@ public class PostApplication {
 	 }
 	
 	@Bean
-	public CommandLineRunner studentDemo(PostRepository prepository, TagRepository trepository, PostStatusRepository psrepository, RatingRepository rarepository) {
+	public CommandLineRunner blogDemo(PostRepository prepository, TagRepository trepository, PostStatusRepository psrepository, RatingRepository rarepository, CommentRepository crepository) {
 		return (args) -> {
 			log.info("testdata");
 
@@ -84,6 +84,150 @@ public class PostApplication {
 			prepository.save(new Post(null ,"Pekka","päivitys 8","",psrepository.findByStatus("Complete").get(0)));
 			prepository.save(new Post(null ,"Aino","päivitys 7","",psrepository.findByStatus("WIP").get(0)));
 			prepository.save(new Post(null ,"Pekka","päivitys 9","",psrepository.findByStatus("Complete").get(0)));
+
+			//testi kommentit
+			Comment comment1 = crepository.save(new Comment("Comment 1"));
+			Comment comment2 = crepository.save(new Comment("Comment 2"));
+			Comment comment3 = crepository.save(new Comment("Comment 3"));
+			Comment comment4 = crepository.save(new Comment("Comment 4"));
+			Comment comment5 = crepository.save(new Comment("Comment 5"));
+			Comment comment6 = crepository.save(new Comment("Comment 6"));
+			Comment comment7 = crepository.save(new Comment("Comment 7"));
+			Comment comment8 = crepository.save(new Comment("Comment 8"));
+			Comment comment9 = crepository.save(new Comment("Comment 9"));
+			Comment comment10 = crepository.save(new Comment("Comment 10"));
+			Comment comment11 = crepository.save(new Comment("Comment 11"));
+			Comment comment12 = crepository.save(new Comment("Comment 12"));
+			Comment comment13 = crepository.save(new Comment("Comment 13"));
+			Comment comment14 = crepository.save(new Comment("Comment 14"));
+			Comment comment15 = crepository.save(new Comment("Comment 15"));
+			Comment comment16 = crepository.save(new Comment("Comment 16"));
+			Comment comment17 = crepository.save(new Comment("Comment 17"));
+			Comment comment18 = crepository.save(new Comment("Comment 18"));
+			Comment comment19 = crepository.save(new Comment("Comment 19"));
+			Comment comment20 = crepository.save(new Comment("Comment 20"));
+			Comment comment21 = crepository.save(new Comment("Comment 21"));
+			Comment comment22 = crepository.save(new Comment("Comment 22"));
+			Comment comment23 = crepository.save(new Comment("Comment 23"));
+			Comment comment24 = crepository.save(new Comment("Comment 24"));
+			Comment comment25 = crepository.save(new Comment("Comment 25"));
+			Comment comment26 = crepository.save(new Comment("Comment 26"));
+			Comment comment27 = crepository.save(new Comment("Comment 27"));
+			Comment comment28 = crepository.save(new Comment("Comment 28"));
+			Comment comment29 = crepository.save(new Comment("Comment 29"));
+			Comment comment30 = crepository.save(new Comment("Comment 30"));
+			Comment comment31 = crepository.save(new Comment("Comment 31"));
+			Comment comment32 = crepository.save(new Comment("Comment 32"));
+			Comment comment33 = crepository.save(new Comment("Comment 33"));
+			Comment comment34 = crepository.save(new Comment("Comment 34"));
+			Comment comment35 = crepository.save(new Comment("Comment 35"));
+
+			//post comments testi data
+			post1.getComments().add(comment1);
+			comment1.getPosts().add(post1);
+
+			post1.getComments().add(comment2);
+			comment2.getPosts().add(post1);
+
+			post1.getComments().add(comment3);
+			comment3.getPosts().add(post1);
+
+			post1.getComments().add(comment4);
+			comment4.getPosts().add(post1);
+
+			post1.getComments().add(comment5);
+			comment5.getPosts().add(post1);
+
+			post1.getComments().add(comment6);
+			comment6.getPosts().add(post1);
+
+			post1.getComments().add(comment7);
+			comment7.getPosts().add(post1);
+
+			post1.getComments().add(comment8);
+			comment8.getPosts().add(post1);
+
+			post1.getComments().add(comment9);
+			comment9.getPosts().add(post1);
+
+			post1.getComments().add(comment10);
+			comment10.getPosts().add(post1);
+
+			post1.getComments().add(comment11);
+			comment11.getPosts().add(post1);
+
+			post1.getComments().add(comment12);
+			comment12.getPosts().add(post1);
+
+			post1.getComments().add(comment13);
+			comment13.getPosts().add(post1);
+
+			post1.getComments().add(comment14);
+			comment14.getPosts().add(post1);
+
+			post1.getComments().add(comment15);
+			comment15.getPosts().add(post1);
+
+			post1.getComments().add(comment16);
+			comment16.getPosts().add(post1);
+
+			post1.getComments().add(comment17);
+			comment17.getPosts().add(post1);
+
+			post1.getComments().add(comment18);
+			comment18.getPosts().add(post1);
+
+			post1.getComments().add(comment19);
+			comment19.getPosts().add(post1);
+
+			post1.getComments().add(comment20);
+			comment20.getPosts().add(post1);
+
+			post1.getComments().add(comment21);
+			comment21.getPosts().add(post1);
+
+			post1.getComments().add(comment22);
+			comment22.getPosts().add(post1);
+
+			post1.getComments().add(comment23);
+			comment23.getPosts().add(post1);
+
+			post1.getComments().add(comment24);
+			comment24.getPosts().add(post1);
+
+			post1.getComments().add(comment25);
+			comment25.getPosts().add(post1);
+
+			post1.getComments().add(comment26);
+			comment26.getPosts().add(post1);
+
+			post1.getComments().add(comment27);
+			comment27.getPosts().add(post1);
+
+			post1.getComments().add(comment28);
+			comment28.getPosts().add(post1);
+
+			post1.getComments().add(comment29);
+			comment29.getPosts().add(post1);
+
+			post1.getComments().add(comment30);
+			comment30.getPosts().add(post1);
+
+			post1.getComments().add(comment31);
+			comment31.getPosts().add(post1);
+
+			post1.getComments().add(comment32);
+			comment32.getPosts().add(post1);
+
+			post1.getComments().add(comment33);
+			comment33.getPosts().add(post1);
+
+			post1.getComments().add(comment34);
+			comment34.getPosts().add(post1);
+
+			post1.getComments().add(comment35);
+			comment35.getPosts().add(post1);
+
 
 			//post ratings testi data
 			post1.getRatings().add(rating1);
